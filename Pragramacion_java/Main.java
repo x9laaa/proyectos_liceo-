5db18713f2b25d9208ef533bc79e3c790ffd7f28
x9laaa <acarreno95@outlook.com>
2026-03-23 00:51:49 -0300

    Ajustar la indentación en el método main de la clase Main

--- Pragramacion_java/Main.java
+++ Pragramacion_java/Main.java
@@ -1,8 +1,7 @@
 package Pragramacion_java;
 
 public class Main {
-    
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
