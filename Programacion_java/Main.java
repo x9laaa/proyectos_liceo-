3cbbd40cd343361a89dcb9da13020dd4e75c804d
x9laaa <acarreno95@outlook.com>
2026-05-17 19:56:08 -0400

    Modificar el bucle en la clase Main para limitar la salida a los números del 0 al 10

--- Programacion_java/Main.java
+++ Programacion_java/Main.java
@@ -3,7 +3,7 @@ package Programacion_java;
 public class Main {
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 1000000; i++) {
+        for (int i = 0; i <= 10; i++) {
             System.out.println(i);
         }
 
